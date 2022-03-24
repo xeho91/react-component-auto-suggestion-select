9d7915ff7eaf14136e50ce04d27821a15fd13def
Matt Kadlubowski <xeho91@pm.me>
2022-03-24 12:05:52 +0700

    feat(components): ✨  Add initial files for `AutoSuggestionSelect`

---
+++ source/components/AutoSuggestionSelect.stories.tsx
@@ -0,0 +1,3 @@
+import { AutoSuggestionSelect as Component } from "./AutoSuggestionSelect";
+
+export const AutoSuggestionSelect = () => <Component />;
---
+++ source/components/AutoSuggestionSelect.tsx
@@ -0,0 +1,11 @@
+import type { FunctionComponent } from "react";
+
+import CSS from "./AutoSuggestionSelect.module.scss";
+
+export interface AutoSuggestionSelectProperties {}
+
+export const AutoSuggestionSelect: FunctionComponent<AutoSuggestionSelectProperties> = () => {
+	return <div />;
+};
+AutoSuggestionSelect.displayName = "AutoSuggestionSelect";
+AutoSuggestionSelect.defaultProps = {};
