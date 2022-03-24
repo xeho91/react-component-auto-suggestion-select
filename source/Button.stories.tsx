43afb0dc1851a3f7750c1f016a9cf6ce24c020ea
Matt Kadlubowski <xeho91@pm.me>
2022-03-24 12:05:08 +0700

    chore: 🔥  Remove example file

--- source/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Button as Component } from "./Button";
-
-export const Button = () => (
-	<Component
-		title="Click!"
-		label="Don't you dare to click me"
-		onClick={() => alert("Why did you do this?")}
-	/>
-);
